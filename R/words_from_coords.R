94b58348ca4236548128b8ee7ddff029ef088a23
David Smith <david.alex.smith@gmail.com>
2023-09-21 23:21:35 +0100

    Now vectorised over coordinates

--- R/words_from_coords.R
+++ R/words_from_coords.R
@@ -14,30 +14,41 @@ words_from_coords <- function(lat,
                               language = "en",
                               full_details = FALSE) {
 
+
   # Get API key from environment variable
   key <- whatthreewords::get_api_key()
 
-  # Build request
-  req <- httr2::request("https://api.what3words.com/v3/") |>
-    httr2::req_url_path_append("convert-to-3wa") |>
-    httr2::req_url_query(coordinates = paste0(lat,",",lon),
-                  language = language,
-                  key = key)
+  # Build requests
+  requests <- mapply(function(lat, lon, language, key)
+    httr2::request("https://api.what3words.com/v3/") |>
+      httr2::req_url_path_append("convert-to-3wa") |>
+      httr2::req_url_query(coordinates = paste0(lat,",",lon),
+                           language = language,
+                           key = key),
+    lat = lat,
+    lon = lon,
+    MoreArgs = list(language = language,
+                    key = key),
+    SIMPLIFY = FALSE)
 
-  # Make request
-  resp <-  req |>
-    httr2::req_perform()
+
+  # Make requests
+  responses <-  requests |>
+    lapply(httr2::req_perform)
 
   # Get JSON content of response
-  body <- resp |>
-    httr2::resp_body_json()
+  contents <- responses |>
+    lapply(httr2::resp_body_json)
 
   # Return words or full details
   if(full_details) {
-    out <- body
+    out <- contents
   } else {
-    out <- body$words
+    out <- contents |>
+      lapply(function(x) x$words) |>
+      unlist()
   }
   out
+
 }
 
